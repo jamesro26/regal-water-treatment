69c9d7198f3da405d066b9eae41a6d854653b187
James Robertson <gerry45013@yahoo.com>
2018-02-11 13:50:32 -0500

    First pass at creating nav to overlay on vidoes
    My first attempt at creating nav to overlay on top of the videos to give users more control and freedom to explore the app.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,4 @@
-! function (e) {
+! function(e) {
   function t(n) {
     if (i[n]) return i[n].exports;
     var a = i[n] = {
@@ -9,36 +9,36 @@
     return e[n].call(a.exports, a, a.exports, t), a.l = !0, a.exports
   }
   var i = {};
-  t.m = e, t.c = i, t.d = function (e, i, n) {
+  t.m = e, t.c = i, t.d = function(e, i, n) {
     t.o(e, i) || Object.defineProperty(e, i, {
       configurable: !1,
       enumerable: !0,
       get: n
     })
-  }, t.n = function (e) {
-    var i = e && e.__esModule ? function () {
+  }, t.n = function(e) {
+    var i = e && e.__esModule ? function() {
       return e.default
-    } : function () {
+    } : function() {
       return e
     };
     return t.d(i, "a", i), i
-  }, t.o = function (e, t) {
+  }, t.o = function(e, t) {
     return Object.prototype.hasOwnProperty.call(e, t)
   }, t.p = "", t(t.s = 0)
-}([function (e, t) {
+}([function(e, t) {
   function i() {
     var e = $(".map_container").width(),
-      t = $(".map_container").height(),
-      i = !1;
-    i || (i = !0, window.setTimeout(function () {
+        t = $(".map_container").height(),
+        i = !1;
+    i || (i = !0, window.setTimeout(function() {
       var n = $(".map_container").width(),
-        o = $(".map_container").height();
-      n === e && o === t && function (e, t) {
+          o = $(".map_container").height();
+      n === e && o === t && function(e, t) {
         var i = $("img"),
-          n = i.width(),
-          o = i.height(),
-          r = 0,
-          s = 0;
+            n = i.width(),
+            o = i.height(),
+            r = 0,
+            s = 0;
         n / e > o / t || n / e < o / t ? r = e : s = t, i.mapster("resize", r, s, a)
       }(n, o), i = !1
     }, o))
@@ -57,35 +57,41 @@
     },
     areas: [{
       key: "sanitary",
-      toolTip: "Administration & Maintenance"
+      toolTip: true
     }, {
       key: "pump",
-      toolTip: "Primary Effluent Pump/Lift Station"
+      toolTip: true
     }, {
       key: "basin",
       toolTip: "Primary Settling Basins"
     }, {
       key: "digesters",
-      toolTip: "Anaerobic Digesters"
+      toolTip: true
     }, {
       key: "fermenters",
-      toolTip: "Fermenters"
+      toolTip: true
     }, {
       key: "clarifiers",
-      toolTip: "Primary Clarifiers &amp; Aeration Tanks"
+      toolTip: true
     }, {
       key: "bioreactors",
-      toolTip: "Bioreactor"
+      toolTip: true
     }, {
       key: "degritters",
-      toolTip: "Cyclone Degritters"
+      toolTip: true
     }, {
       key: "secondary_clarifiers",
-      toolTip: "Secondary Clarifiers & Chlorine Tanks"
+      toolTip: true
     }, {
       key: "belt_filter",
-      toolTip: "Digesters & Belt Filter Presses"
-    }]
+      toolTip: true
+    }],
+    showToolTip: true,
+    onShowToolTip: function(data) {
+      if (this.href && this.href!='#') {
+        data.toolTip.html('<img src="'+this.href+'">');
+      }
+    }
   };
   $("#main_map").mapster(n), $("#pump_station").mapster({
     staticState: !1,
@@ -96,38 +102,36 @@
     }
   });
   var a = 0,
-    o = 0,
-    r = navigator.userAgent.indexOf("Edge") > -1 || navigator.userAgent.indexOf("Trident/7.0") > -1;
-  $(window).bind("resize", i), window.toggleView = function (e) {
-    $(e).hasClass("hide") ? ($(".mapster_tooltip").css("display", "none"), $(".map_area").fadeToggle(300, "linear", function () {
+      o = 0,
+      r = navigator.userAgent.indexOf("Edge") > -1 || navigator.userAgent.indexOf("Trident/7.0") > -1;
+  $(window).bind("resize", i), window.toggleView = function(e) {
+    $(e).hasClass("hide") ? ($(".mapster_tooltip").css("display", "none"), $(".map_area").fadeToggle(300, "linear", function() {
       $(e).toggleClass("hide")
     })) : ($(e).toggleClass("hide"), $(".map_area").fadeToggle(300, "linear")), removeBlur(), i()
   };
   var s = document.getElementsByTagName("video")[1],
-    l = document.getElementsByTagName("video")[2],
-    d = document.getElementsByTagName("video")[3];
-  console.log(s, l, d), $("#cabinetModal").on("shown.bs.modal", function () {
-    s.play(), s.addEventListener("ended", function () {
+      l = document.getElementsByTagName("video")[2],
+      d = document.getElementsByTagName("video")[3];
+  console.log(s, l, d), $("#cabinetModal").on("shown.bs.modal", function() {
+    s.play(), s.addEventListener("ended", function() {
       $(".cabinet").addClass("hide"), $(".cabinet_inside").removeClass("hide")
     })
-  }), $("#cabinetModal").on("hidden.bs.modal", function () {
-    s.currentTime = 0, $(".cabinet").toggleClass("hide"), $(".cabinet_inside").toggleClass("hide")
-  }), $("#generatorModal").on("shown.bs.modal", function () {
-    d.play()
-  }), $("#generatorModal").on("hidden.bs.modal", function () {
-    d.currentTime = 0
-  }), $("#fanModal").on("shown.bs.modal", function () {
-    l.play()
-  }), $("#fanModal").on("hidden.bs.modal", function () {
-    l.currentTime = 0
-  }), window.applyBlur = function () {
+  }), window.applyBlur = function() {
     r ? $('img[src$="_map.jpg"]').attr("src", "./assets/images/blurred_main_map.jpg") : $('img[src$="_map.jpg"]').css({
       filter: "blur(3px)",
       transition: "all 0.4s ease"
     })
-  }, window.removeBlur = function (e) {
+  }, window.removeBlur = function(e) {
     r ? $('img[src$="_map.jpg"]').attr("src", "./assets/images/`${main_map}`.jpg") : $('img[src$="_map.jpg"]').css("filter", "")
-  }, $(window).on("hidden.bs.modal", function (e) {
+  }, $(window).on("hidden.bs.modal", function(e) {
     window.scrollTo(0, 0)
-  })
+  }),$(document).ready(function() {
+    $("#nav-overlay li").hover(
+      function() {
+        $(this).find('i').addClass("rotate");
+      },
+      function() {
+        $(this).find('i').removeClass("rotate");
+      })
+  });
 }]);
